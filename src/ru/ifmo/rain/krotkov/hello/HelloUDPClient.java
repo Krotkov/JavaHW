c8337a63dc90b888ad1b20dd04c261be401ec964
Krotkov <krotkovd99@gmail.com>
2019-05-21 14:01:02 +0300

    Fix

--- src/ru/ifmo/rain/krotkov/hello/HelloUDPClient.java
+++ src/ru/ifmo/rain/krotkov/hello/HelloUDPClient.java
@@ -19,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class HelloUDPClient implements HelloClient {
-    private static final int SOCKET_SO_TIMEOUT = 500; //milliseconds
+    private static final int SOCKET_SO_TIMEOUT = 500;
 
     @Override
     public void run(String host, int port, String prefix, int threads, int requests) {
@@ -35,44 +35,42 @@ public class HelloUDPClient implements HelloClient {
             return;
         }
 
-        final SocketAddress dst = new InetSocketAddress(addr, port);
+        final SocketAddress socketAddress = new InetSocketAddress(addr, port);
         final ExecutorService workers = Executors.newFixedThreadPool(threads);
 
-        IntStream.range(0, threads).forEach(id -> workers.submit(() -> sendAndReceive(dst, prefix, requests, id)));
+        IntStream.range(0, threads).forEach(id -> workers.submit(() -> {
+            try (DatagramSocket socket = new DatagramSocket()) {
+                socket.setSoTimeout(SOCKET_SO_TIMEOUT);
+                final byte[] buffer = new byte[socket.getReceiveBufferSize()];
+                final DatagramPacket response = new DatagramPacket(buffer, socket.getReceiveBufferSize());
+                for (int num = 0; num < requests; num++) {
+                    final String requestText = prefix + id + "_" + num;
+                    byte[] dataRequest = requestText.getBytes(StandardCharsets.UTF_8);
+                    DatagramPacket msg = new DatagramPacket(dataRequest, requestText.length(), socketAddress);
+                    while (!socket.isClosed() || Thread.currentThread().isInterrupted()) {
+                        try {
+                            socket.send(msg);
+                            System.out.println("Request sent:\n" + requestText + "\n");
+                            socket.receive(response);
+                            String responseText = new String(response.getData(), 0, response.getLength(), StandardCharsets.UTF_8);
+                            if (responseText.contains(requestText)) {
+                                System.out.println("Response received:\n" + responseText + "\n");
+                                break;
+                            }
+                        } catch (IOException e) {
+                            System.err.println("Error while processing request: " + e.getMessage());
+                        }
+                    }
+                }
+            } catch (SocketException e) {
+                System.err.println("Can't create socket: " + addr.toString());
+            }
+        }));
 
         workers.shutdown();
         try {
             workers.awaitTermination(requests * 5, TimeUnit.SECONDS); // ten tries per request
         } catch (InterruptedException ignored) {
-        }
-    }
-
-    private static void sendAndReceive(final SocketAddress addr, final String prefix, int requests, int id) {
-        try (DatagramSocket socket = new DatagramSocket()) {
-            socket.setSoTimeout(SOCKET_SO_TIMEOUT);
-            final byte[] buffer = new byte[socket.getReceiveBufferSize()];
-            final DatagramPacket response = new DatagramPacket(buffer, socket.getReceiveBufferSize());
-            for (int num = 0; num < requests; num++) {
-                final String requestText = prefix + id + "_" + num;
-                byte[] dataRequest = requestText.getBytes(StandardCharsets.UTF_8);
-                DatagramPacket msg = new DatagramPacket(dataRequest, requestText.length(), addr);
-                while (!socket.isClosed() || Thread.currentThread().isInterrupted()) {
-                    try {
-                        socket.send(msg);
-                        System.out.println("Request sent:\n" + requestText + "\n");
-                        socket.receive(response);
-                        String responseText = new String(response.getData(), 0, response.getLength(), StandardCharsets.UTF_8);
-                        if (responseText.contains(requestText)) {
-                            System.out.println("Response received:\n" + responseText + "\n");
-                            break;
-                        }
-                    } catch (IOException e) {
-                        System.err.println("Error occurred while processing request: " + e.getMessage());
-                    }
-                }
-            }
-        } catch (SocketException e) {
-            System.err.println("Unable to create socket: " + addr.toString());
         }
     }
 
